3ebed4dca45b0464d33f2a441f630df37a685a45
Jan Buntenbroich <jan@buntenbroich.de>
2025-11-02 17:25:56 +0100

    h1 fertig

---
+++ src/h1/H1_main.java
@@ -0,0 +1,43 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[] args) {
+		double guthaben = -100, monEingang = 200;
+		int rating = -2;
+		boolean warnhinweis = false, negativ = false;
+		
+		if (guthaben < 0) {
+			negativ = true;
+		}else {
+			negativ = false;
+		}
+		if (guthaben > 0) {
+			rating = rating + 3;
+		}	
+		if (guthaben == 0) {
+			rating = rating + 2;
+		}
+		if (guthaben < 0) {
+			if (Math.abs(monEingang) >= Math.abs(guthaben)) {
+				rating = rating + 1;
+		}
+		}	
+		if (guthaben < 0) {
+			if (Math.abs(monEingang) < Math.abs(guthaben)) {
+				rating = rating - 1;
+		}
+		}	
+		if (guthaben < 0) {
+			if (Math.abs(monEingang) < Math.abs(guthaben)) {
+				if (rating < 0) {
+					warnhinweis = true;
+				} else {
+					warnhinweis = false;
+		}	
+		}
+		}	
+	System.out.println("Negativ? " + negativ);	
+	System.out.println("Rating: " + rating);
+	System.out.println("warnhinweis? " + warnhinweis);
+	}
+}		
