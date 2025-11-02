0fe0c365f8b7ac5d3277f930760d53af21919f24
Jan Buntenbroich <jan@buntenbroich.de>
2025-11-02 11:57:13 +0100

    p1 fertig

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -2,7 +2,28 @@ package p1;
 
 public class P1_main {
 	public static void main(String[] args) {
+		int wuerfel = 1;
+		
+		if (wuerfel == 1) {
+			System.out.println ("Gewonnen!");
+		} else {
+			if (wuerfel == 4) {
+				System.out.println ("Gewonnen!");
+			} else {
+				if (wuerfel == 2) {
+					System.out.println ("Verloren!");
+				} else {
+					if (wuerfel == 5) {
+						System.out.println ("Unentschieden!");
+					} else {
+						System.out.println ("Unklar!");
+						
+					}
+				}
+			}
+		}
+		
+		
 		
 	}
-
-}
+}			
